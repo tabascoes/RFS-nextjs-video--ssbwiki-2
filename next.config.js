d9d3a7a80a2867fe230333b57396ca375624e967
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-10 12:26:05 +0900

    Changed `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const withPlugins = require('next-compose-plugins')
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
+const withVideos = require('next-videos')
 
 // i18n
-module.exports = withNextra({
+const i18n = withNextra({
   i18n: {
     locales: ['jp', 'en'],
     defaultLocale: 'jp',
   },
 })
+
+const video = withVideos()
+
+module.exports = withPlugins([i18n, video])
